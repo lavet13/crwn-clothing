dc31eb313e5b1d4afd2202bcb635bb032f8ae44e
lavet13 <lavet13@mail.ru>
2023-04-09 19:53:58 +0300

    category-menu & category-item

--- src/App.js
+++ src/App.js
@@ -1,41 +1,7 @@
-import './categories.styles.scss';
+import CategoriesMenu from './components/categories-menu/categories-menu.component';
 
 const App = () => {
-  const categories = [
-    {
-      id: '1',
-      title: 'Hats',
-    },
-    {
-      id: '2',
-      title: 'Jackets',
-    },
-    {
-      id: '3',
-      title: 'Sneakers',
-    },
-    {
-      id: '4',
-      title: 'Womens',
-    },
-    {
-      id: '5',
-      title: 'Mens',
-    },
-  ];
-  return (
-    <div className='categories-container'>
-      {categories.map(({ id, title }) => (
-        <div key={id} className='category-container'>
-          <div className='background-image' />
-          <div className='category-body-container'>
-            <h4>{title}</h4>
-            <p>Shop Now</p>
-          </div>
-        </div>
-      ))}
-    </div>
-  );
+  return <CategoriesMenu />;
 };
 
 export default App;
