5b054db84ffaaa99918cb79ff0d267892522b4e0
lavet13 <lavet13@mail.ru>
2023-04-09 12:29:46 +0300

    converting our data in UI

--- src/App.js
+++ src/App.js
@@ -1,41 +1,37 @@
 const App = () => {
+  const categories = [
+    {
+      id: '1',
+      title: 'Hats',
+    },
+    {
+      id: '2',
+      title: 'Jackets',
+    },
+    {
+      id: '3',
+      title: 'Sneakers',
+    },
+    {
+      id: '4',
+      title: 'Womens',
+    },
+    {
+      id: '5',
+      title: 'Mens',
+    },
+  ];
   return (
     <div className='categories-container'>
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h4>Hats</h4>
-          <p>Shop Now</p>
+      {categories.map(({ id, title }) => (
+        <div key={id} className='category-container'>
+          <div className='background-image' />
+          <div className='category-body-container'>
+            <h4>{title}</h4>
+            <p>Shop Now</p>
+          </div>
         </div>
-      </div>
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h4>Jackets</h4>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h4>Sneakers</h4>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h4>Womens</h4>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className='category-container'>
-        {/* <img /> */}
-        <div className='category-body-container'>
-          <h4>Mens</h4>
-          <p>Shop Now</p>
-        </div>
-      </div>
+      ))}
     </div>
   );
 };
