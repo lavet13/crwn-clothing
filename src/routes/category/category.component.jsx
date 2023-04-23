f68a00fb5b4f38357745aab1f5c31e6cf5cdbd51
lavet13 <lavet13@mail.ru>
2023-04-23 20:47:35 +0300

    category route

---
+++ src/routes/category/category.component.jsx
@@ -0,0 +1,30 @@
+import { useContext, useState, useEffect } from 'react';
+
+import { CategoriesContext } from '../../contexts/categories.context';
+
+import { useParams } from 'react-router-dom';
+
+import ProductCard from '../../components/product-card/product-card.component';
+
+import './category.styles.scss';
+
+const Category = () => {
+  const { category } = useParams();
+  const { categoriesMap } = useContext(CategoriesContext);
+  const [products, setProducts] = useState(categoriesMap[category]);
+
+  useEffect(() => {
+    const products = categoriesMap[category];
+    setProducts(products);
+  }, [category, categoriesMap]);
+
+  return (
+    <div className='category-page-container'>
+      {products?.map(product => (
+        <ProductCard key={product.id} product={product} />
+      ))}
+    </div>
+  );
+};
+
+export default Category;
