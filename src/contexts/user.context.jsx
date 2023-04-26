48993235db956328e55e63e9276dc148d41793c0
lavet13 <lavet13@mail.ru>
2023-04-26 23:38:59 +0300

    userReducer

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState, useEffect } from 'react';
+import { createContext, useEffect, useReducer } from 'react';
 
 import {
   onAuthStateChangedListener,
@@ -11,8 +11,37 @@ export const UserContext = createContext({
   setCurrentUser: () => null,
 }); // passing kind of default value, not necessary the initial value
 
+export const USER_ACTION_TYPES = {
+  SET_CURRENT_USER: 'SET_CURRENT_USER',
+};
+
+const userReducer = (state, action) => {
+  const { type, payload } = action;
+
+  switch (type) {
+    case USER_ACTION_TYPES.SET_CURRENT_USER:
+      return { ...state, currentUser: payload };
+    default:
+      throw new Error(`Unhandled type ${type} in userReducer`);
+  }
+};
+
+const INITIAL_STATE = {
+  currentUser: null,
+};
+
 // this provider is essentially allowing any of it's child components to access the values inside useState
 export const UserProvider = ({ children }) => {
+  const [{ currentUser }, dispatch] = useReducer(userReducer, INITIAL_STATE);
+
+  const setCurrentUser = user =>
+    dispatch({ type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user });
+
+  const value = {
+    currentUser,
+    setCurrentUser,
+  };
+
   useEffect(() => {
     const unsubscribe = onAuthStateChangedListener(user => {
       if (user) {
@@ -25,12 +54,15 @@ export const UserProvider = ({ children }) => {
     return unsubscribe;
   }, []);
 
-  const [currentUser, setCurrentUser] = useState(null);
-
-  const value = {
-    currentUser,
-    setCurrentUser,
-  };
-
   return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
 };
+
+/**
+  const userReducer = (state, action) => {
+    return {
+      currentUser: null
+    };
+  };
+
+
+ */
