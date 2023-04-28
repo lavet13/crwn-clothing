4782203d7a6de8f6158be5275b19b63d9faae31a
lavet13 <lavet13@mail.ru>
2023-04-29 01:10:50 +0300

    categories.context bye

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -2,12 +2,11 @@ import { useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 
-import { setCategoriesMap } from '../../store/categories/categories.action';
-
 import CategoriesPreview from '../categories-preview/categories-preview.component';
 import Category from '../category/category.component';
 
 import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils';
+import { setCategoriesMap } from '../../store/categories/categories.action';
 
 const Shop = () => {
   const dispatch = useDispatch();
