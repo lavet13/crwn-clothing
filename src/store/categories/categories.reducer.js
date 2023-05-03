5692d451feea0a053df03645178a4063fb358cf4
lavet13 <lavet13@mail.ru>
2023-05-04 01:05:54 +0300

    setting up categories for thunk middleware

--- src/store/categories/categories.reducer.js
+++ src/store/categories/categories.reducer.js
@@ -2,6 +2,8 @@ import { CATEGORIES_ACTION_TYPES } from './categories.types';
 
 export const CATEGORIES_INITIAL_STATE = {
   categories: [],
+  isLoading: false,
+  error: null,
 };
 
 // reducer should always store the most basic format, essentially the data you get from your API
@@ -12,8 +14,12 @@ export const categoriesReducer = (
   const { type, payload } = action;
 
   switch (type) {
-    case CATEGORIES_ACTION_TYPES.SET_CATEGORIES:
-      return { ...state, categories: payload };
+    case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START:
+      return { ...state, isLoading: true };
+    case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS:
+      return { ...state, categories: payload, isLoading: false };
+    case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED:
+      return { ...state, error: payload, isLoading: false };
     default:
       return state;
   }
