8bbe06d23ae48a483f414ed35e95ab715a58480f
lavet13 <lavet13@mail.ru>
2023-05-21 05:04:37 +0300

    withMatcher

--- src/store/categories/categories.reducer.ts
+++ src/store/categories/categories.reducer.ts
@@ -1,6 +1,12 @@
-import { CATEGORIES_ACTION_TYPES, Category } from './categories.types';
+import { AnyAction } from 'redux';
 
-import { CategoryAction } from './categories.action';
+import { Category } from './categories.types';
+
+import {
+  fetchCategoriesStart,
+  fetchCategoriesSuccess,
+  fetchCategoriesFailed,
+} from './categories.action';
 
 export type CategoriesState = {
   readonly categories: Category[];
@@ -17,16 +23,19 @@ export const CATEGORIES_INITIAL_STATE: CategoriesState = {
 // reducer should always store the most basic format, essentially the data you get from your API
 export const categoriesReducer = (
   state = CATEGORIES_INITIAL_STATE,
-  action = {} as CategoryAction
+  action = {} as AnyAction
 ): CategoriesState => {
-  switch (action.type) {
-    case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START:
-      return { ...state, isLoading: true };
-    case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS:
-      return { ...state, categories: action.payload, isLoading: false };
-    case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED:
-      return { ...state, error: action.payload, isLoading: false };
-    default:
-      return state;
+  if (fetchCategoriesStart.match(action)) {
+    return { ...state, isLoading: true };
   }
+
+  if (fetchCategoriesSuccess.match(action)) {
+    return { ...state, categories: action.payload, isLoading: false };
+  }
+
+  if (fetchCategoriesFailed.match(action)) {
+    return { ...state, error: action.payload, isLoading: false };
+  }
+
+  return state;
 };
