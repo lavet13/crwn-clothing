ad0667926ac0c82c2a86f6cea98d3cae8e801fe5
lavet13 <lavet13@mail.ru>
2023-05-27 04:48:21 +0300

    user-reducer & user-selector typing

---
+++ src/store/user/user.reducer.ts
@@ -0,0 +1,60 @@
+import { AnyAction } from 'redux';
+
+import { SignInSuccessPayload } from './user.types';
+
+import {
+  checkUserSession,
+  emailSignInStart,
+  googleSignInStart,
+  signInFailed,
+  signInSuccess,
+  signOutFailed,
+  signOutStart,
+  signOutSuccess,
+  signUpFailed,
+  signUpStart,
+} from './user.action';
+
+export type UserState = {
+  readonly currentUser: SignInSuccessPayload | null;
+  readonly isLoading: boolean;
+  readonly error: Error | null;
+};
+
+const USER_INITIAL_STATE: UserState = {
+  currentUser: null,
+  isLoading: false,
+  error: null,
+};
+
+export const userReducer = (
+  state = USER_INITIAL_STATE,
+  action: AnyAction
+): UserState => {
+  if (
+    checkUserSession.match(action) ||
+    googleSignInStart.match(action) ||
+    emailSignInStart.match(action) ||
+    signUpStart.match(action) ||
+    signOutStart.match(action)
+  )
+    return { ...state, isLoading: true };
+
+  if (signOutSuccess.match(action)) {
+    return { ...state, currentUser: null, isLoading: false };
+  }
+
+  if (signInSuccess.match(action)) {
+    return { ...state, currentUser: action.payload, isLoading: false };
+  }
+
+  if (
+    signInFailed.match(action) ||
+    signUpFailed.match(action) ||
+    signOutFailed.match(action)
+  ) {
+    return { ...state, error: action.payload, isLoading: false };
+  }
+
+  return state;
+};
