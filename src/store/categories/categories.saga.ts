c767c05c02e5567145e24825d77b08bbe9179760
lavet13 <lavet13@mail.ru>
2023-06-01 04:15:47 +0300

    categories-saga

--- src/store/categories/categories.saga.ts
+++ src/store/categories/categories.saga.ts
@@ -1,6 +1,6 @@
 // 4 saga based effect generators; saga have redux bindings on them
 // (able to access the redux store, able to dispatch to it, we do so through these methods)
-import { takeLatest, all, call, put } from 'redux-saga/effects';
+import { takeLatest, all, call, put } from 'typed-redux-saga';
 
 import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils';
 
@@ -13,21 +13,21 @@ import { CATEGORIES_ACTION_TYPES } from './categories.types';
 
 export function* fetchCategoriesAsync() {
   try {
-    const categoriesArray = yield call(getCategoriesAndDocuments);
-    yield put(fetchCategoriesSuccess(categoriesArray));
+    const categoriesArray = yield* call(getCategoriesAndDocuments);
+    yield* put(fetchCategoriesSuccess(categoriesArray));
   } catch (error) {
-    yield put(fetchCategoriesFailed(error));
+    yield* put(fetchCategoriesFailed(error as Error));
   }
 }
 
 export function* onFetchCategories() {
   // where we receive actions, means if you have a bunch of the same action give me the latest one
-  yield takeLatest(
+  yield* takeLatest(
     CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START,
     fetchCategoriesAsync
   );
 }
 
 export function* categoriesSaga() {
-  yield all([call(onFetchCategories)]); // run everything inside and only complete when all of it is done
+  yield* all([call(onFetchCategories)]); // run everything inside and only complete when all of it is done
 }
