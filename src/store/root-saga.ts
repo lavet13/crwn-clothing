f818640be9a04d0a72a86fcb77871813150d4228
lavet13 <lavet13@mail.ru>
2023-06-04 02:52:06 +0300

    typing

--- src/store/root-saga.ts
+++ src/store/root-saga.ts
@@ -1,9 +1,9 @@
-import { all, call } from 'redux-saga/effects';
+import { all, call } from 'typed-redux-saga/macro';
 
 import { categoriesSaga } from './categories/categories.saga';
 import { userSagas } from './user/user.saga';
 import { paymentSagas } from './payment/payment.saga';
 
 export function* rootSaga() {
-  yield all([call(categoriesSaga), call(userSagas), call(paymentSagas)]);
+  yield* all([call(categoriesSaga), call(userSagas), call(paymentSagas)]);
 }
